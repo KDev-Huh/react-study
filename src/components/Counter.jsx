9103e9a8d0df09b660f55ba42ce0a67a46334ce0
huhonmcmc <huhonmcmc@gmail.com>
2025-12-28 17:04:47 +0900

    12강 함수 표현식 vs 함수 선언식 :: 실습코드

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,15 @@
-function Counter() {
+// 일반적으로 사용
+export default function Counter() {
   return (
     <button>Counter</button>
   )
 }
 
-export default Counter;
+// UI만 담당하는 컴포넌트
+// export const Counter = () => (
+//   <button>Counter</button>
+// )
+
+// export default Counter;
+
+// 함수 선언식 vs 함수 표현식
