78a3531aadbe9d76c02db0350a9742e4646c70d0
huhonmcmc <huhonmcmc@gmail.com>
2026-01-05 20:21:25 +0900

    14강 컴포넌트에 props 전달하기 :: 실습코드

--- src/2. Course List/components/Course.jsx
+++ src/2. Course List/components/Course.jsx
@@ -1,10 +1,11 @@
-export default function Course() {
+export default function Course({title, description, thumbnail}) {
+
   return (
     <article className="course">
-      <img className="course__img" src="./img/htmlcss.png" alt="" />
+      <img className="course__img" src={thumbnail} alt="강의 이미지" />
       <div className="course__body">
-        <div className="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
-        <div className="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+        <div className="course__title">{title}</div>
+        <div className="course__description">{description}</div>
       </div>
     </article>
   )
--- src/2. Course List/components/CourseList.jsx
+++ src/2. Course List/components/CourseList.jsx
@@ -1,11 +1,17 @@
+import Card from './Card.jsx';
 import Course from './Course.jsx';
 
-export default function CourseList() {
+export default function CourseList({items}) {
+
+  const [course1, course2, course3] = items;
+
   return (
-    <div className="courses">
-      <Course />
-      <Course />
-      <Course />
-    </div>
+    <Card title="강의 목록">
+      <div className="courses">
+        <Course {...course1} />
+        <Course {...course2} />
+        <Course {...course3} />
+      </div>
+    </Card>
   )
 }
