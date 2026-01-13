37e16a2afeef2e5110e2742f8d92f14178a04c66
huhonmcmc <huhonmcmc@gmail.com>
2026-01-13 18:27:37 +0900

    25강 State: 컴포넌트의 기억 저장소 :: 실습코드

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,21 @@
 import Counter from "./Counter";
+import { useState } from 'react';
 
 export default function Main() {
+
+  const [total, setTotal] = useState(0);
+
+  const handleTotal = () => {
+    setTotal(total + 1);
+  }
+
   return (
     <main>
-      <Counter />
+      <h2>total: {total}</h2>
+      <Counter onTotal={handleTotal} />
+      <br />
+      <br />
+      <Counter onTotal={handleTotal} />
     </main>
   )
 }
